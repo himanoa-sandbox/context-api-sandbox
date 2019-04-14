fd56b4dccaa80890d2550948e881e15f0b239cf5
himanoa <matsunoappy@gmail.com>
2019-04-14 22:14:10 +0900

    Implemented counter

--- index.tsx
+++ index.tsx
@@ -1,18 +1,30 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom"
-
-import { Store, Provider } from "./store"
+import * as React  from "react"
 import { useContext } from "react";
+import * as ReactDOM from "react-dom"
+import { StoreContext, DispatchContext, Provider, actionCreators } from "./store"
 
 function Counter() {
-  const {state, dispatch} = useContext(Store)
+  const state = useContext(StoreContext)
+  const dispatch = useContext(DispatchContext)
+  return (
+    <>
+      <p> count: { state.count }</p>
+      <button onClick={() => dispatch(actionCreators.increment())}>+</button>
+      <button onClick={() => dispatch(actionCreators.decrement())}>-</button>
+    </>
+  )
+}
 
+function App() {
+  return (
+    <Provider><Counter /></Provider>
+  )
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("app")
   if(root) {
-    ReactDOM.render(<h1>HelloWorld</h1>, root);
+    ReactDOM.render(<App />, root);
   }
 })
 
--- store.tsx
+++ store.tsx
@@ -1,25 +1,36 @@
-import { useReducer, ReactComponentElement } from "react"
+import { useReducer, Reducer, ReducerAction, Dispatch, ReactComponentElement } from "react"
 import * as React  from "react"
 import { createAggregate } from 'redux-aggregate'
 
-type State = {
+interface State {
   count: number
 }
 
-const initialState = (): State => ({ count: 0 })
+const initialState = { count: 0 }
 
 const increment = (state: State) => ({...state, ...{count: state.count + 1}})
 
 const decrement = (state: State) => ({...state, ...{count: state.count - 1}})
 
-const { reducerFactory } = createAggregate({
+const { reducerFactory, creators } = createAggregate({
   increment,
   decrement
 }, "counter/")
 
-export const Store = React.createContext(null as any)
+export const actionCreators = creators
 
-export const Provider = (children: (props: State) => JSX.Element) => {
+export const StoreContext = React.createContext<State>(initialState)
+export const DispatchContext = React.createContext<Dispatch<any>>(null as any)
+
+export const Provider = ({children}) => {
   const [state, dispatch] = useReducer(reducerFactory(initialState), initialState)
-  return <Store.Provider value={{state, dispatch}}>{children}</Store.Provider>
+  return (
+    <StoreContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        {children}
+      </DispatchContext.Provider>
+    </StoreContext.Provider>
+  )
 }
+
+export default {}
